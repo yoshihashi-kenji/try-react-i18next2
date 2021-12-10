8063f6eb6f870f8c830a42bb11ec03d2e04ee893
Kenji Yoshihashi <yoshihashi.kenji@commmune.jp>
2021-12-10 19:50:44 +0900

    I18n page-level function component

--- pages/home-function.js
+++ pages/home-function.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import i18n from '../i18n';
+import { useTranslation } from 'react-i18next';
 
 import FunctionComponent from './FunctionComponent';
 import ClassComponent from './ClassComponent';
@@ -12,6 +13,7 @@ function HomeFunction() {
   if (i18n.language !== locale) {
      i18n.changeLanguage(locale)
   }
+  const { t } = useTranslation(['view']);
   return (
     <div className={styles.container} suppressHydrationWarning>
       <ClassComponent />
@@ -25,57 +27,10 @@ function HomeFunction() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Function</a>
+        {t('description.part2')} <a href="https://nextjs.org">Function</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        </main>
     </div>
   )
 }
