5fc6c877fb9f6a5f7c30110dbc4d8279792139cf
Kenji Yoshihashi <yoshihashi.kenji@commmune.jp>
2021-12-10 19:50:10 +0900

    I18n page-level class component

--- pages/home-class.js
+++ pages/home-class.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import i18n from '../i18n';
+import { withTranslation } from 'react-i18next';
 
 import FunctionComponent from './FunctionComponent';
 import LegacyComponentClass from './ClassComponent';
@@ -15,6 +16,7 @@ class HomeClass extends Component {
       }
 
       render () {
+        const { t } = this.props;
         const locale = 'en'
         if (i18n.language !== locale) {
             i18n.changeLanguage(locale)
@@ -32,7 +34,7 @@ class HomeClass extends Component {
       
             <main className={styles.main}>
               <h1 className={styles.title}>
-                Welcome to <a href="https://nextjs.org">Class</a>
+                {t('description.part2')} <a href="https://nextjs.org">Class</a>
               </h1>
       
               <p className={styles.description}>
@@ -40,39 +42,10 @@ class HomeClass extends Component {
                 <code className={styles.code}>pages/index.js</code>
               </p>
       
-              <div className={styles.grid}>
-                <a href="https://nextjs.org/docs" className={styles.card}>
-                  <h2>Documentation &rarr;</h2>
-                  <p>Find in-depth information about Next.js features and API.</p>
-                </a>
-      
-                <a href="https://nextjs.org/learn" className={styles.card}>
-                  <h2>Learn &rarr;</h2>
-                  <p>Learn about Next.js in an interactive course with quizzes!</p>
-                </a>
-      
-                <a
-                  href="https://github.com/vercel/next.js/tree/master/examples"
-                  className={styles.card}
-                >
-                  <h2>Examples &rarr;</h2>
-                  <p>Discover and deploy boilerplate example Next.js projects.</p>
-                </a>
-      
-                <a
-                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                  className={styles.card}
-                >
-                  <h2>Deploy &rarr;</h2>
-                  <p>
-                    Instantly deploy your Next.js site to a public URL with Vercel.
-                  </p>
-                </a>
-              </div>
             </main>
       
           </div>
         )
     }
 }
-export default HomeClass
+export default withTranslation(['view'])(HomeClass)
